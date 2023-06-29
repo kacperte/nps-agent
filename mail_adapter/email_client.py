daaff12a1cb4ec30957fc7fb4ef8884b5ca6f970
Kacper <k.trzepiecinski@gmail.com>
2023-06-29 22:36:32 +0200

    Improve email client
    This commit introduces several improvements to the MailClient class:
    
    1. Error handling: Added try/except blocks around SMTP operations that could fail, such as login and sendmail.
    
    2. SMTP connection management: Moved the creation of the SMTP connection to the login method and ensured that the connection is closed after sending an email.
    
    3. Database recording: Added a method to record sent emails in a PostgreSQL database.
    
    4. Asynchronicity: Made the MailClient asynchronous using the aiosmtplib library.

--- mail_adapter/email_client.py
+++ mail_adapter/email_client.py
@@ -1,7 +1,9 @@
-import smtplib
+import datetime
+import aiosmtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from databases import Database
+from datetime import datetime
 
 DATABASE_URL = "postgresql://user:password@localhost:5432/mydatabase"
 database = Database(DATABASE_URL)
@@ -9,18 +11,31 @@ database = Database(DATABASE_URL)
 
 class MailClient:
     def __init__(self, host: str, port: int, username: str, password: str):
+        self.host = host
+        self.port = port
         self.username = username
         self.password = password
-        self.server = smtplib.SMTP_SSL(host, port)
-        self.login()
+        self.server = aiosmtplib.SMTP(self.host, self.port, use_tls=True)
 
-    def login(self):
-        self.server.ehlo()
-        self.server.login(self.username, self.password)
+    async def login(self):
+        try:
+            await self.server.connect()
+            await self.server.login(self.username, self.password)
+        except aiosmtplib.SMTPException as e:
+            print(f"Failed to login: {e}")
+            raise
 
-    def send_mail(self, recipient_email: str, subject: str, content):
-        message = self._compose_message(content, recipient_email, subject)
-        self.server.sendmail(self.username, recipient_email, message.as_string())
+    async def send_mail(self, recipient_email: str, subject: str, content, project_name):
+        await self.login()
+        try:
+            message = self._compose_message(content, recipient_email, subject)
+            await self.server.send_message(message)
+            await self.add_record_to_db(email_id=recipient_email, date=datetime.now(), projectname=project_name)
+        except aiosmtplib.SMTPException as e:
+            print(f"Failed to send email: {e}")
+            raise
+        finally:
+            await self.server.quit()
 
     def _compose_message(self, content, recipient_email, subject):
         message = MIMEMultipart("alternative")
@@ -31,5 +46,7 @@ class MailClient:
 
         return message
 
-    def __del__(self):
-        self.server.close()
+    @staticmethod
+    async def add_record_to_db(email_id, date, projectname):
+        query = "INSERT INTO sent_emails (email_id, date, project) VALUES (:email_id,:date, :project)"
+        await database.execute(query=query, values={"email_id": email_id, "date": date, "project": projectname})
