09a6178add212d425c8e3b46128a6a65f3298a8b
Kacper <k.trzepiecinski@gmail.com>
2023-06-29 22:52:41 +0200

    git commit -m "Enhance Message class
    This commit introduces several improvements to the Message class:
    
    1. Parameterized template: The template file is now passed as an argument to the constructor, making the class more flexible.
    
    2. Error handling: Added error handling for the case where the template file does not exist.
    
    3. Configurable template path: The path to the template file can now be passed as an argument to the constructor.
    
    4. Auto-escaping: Enabled auto-escaping in Jinja2 to prevent XSS attacks when templates are user-generated.
    
    These changes make the Message class more robust, secure, and flexible."

--- mail_adapter/messages_parser.py
+++ mail_adapter/messages_parser.py
@@ -2,10 +2,13 @@ import jinja2
 
 
 class Message:
-    def __init__(self):
-        template_loader = jinja2.FileSystemLoader(searchpath="./")
-        self.template_env = jinja2.Environment(loader=template_loader)
-        self.template = self.template_env.get_template(self.TEMPLATE_FILE)
+    def __init__(self, template_file, template_path="./"):
+        template_loader = jinja2.FileSystemLoader(searchpath=template_path)
+        self.template_env = jinja2.Environment(loader=template_loader,autoescape=True)
+        try:
+            self.template = self.template_env.get_template(template_file)
+        except jinja2.TemplateNotFound:
+            print(f"Template {template_file} not found.")
 
     def render(self, **kwargs):
         return self.template.render(**kwargs)
